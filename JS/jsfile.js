11d9446a188f920f78e65cc812e81d2dd4bcbca1
gauravsingh5009 <2000gauravjnp@gmail.com>
2024-06-30 15:07:37 +0530

    Update jS

--- JS/jsfile.js
+++ JS/jsfile.js
@@ -1,13 +1,40 @@
 $(function(){
+
     $(".toggle").click(function(){
         $(this).toggleClass("active");
         $(".collapse").slideToggle()
     });
-
     if(screen.width<=980){
-        $(".collapse ul li").click(function(e){
+        $(".collapse ul li").click(function(){
             $(".collapse").slideUp();
             $(".toggle").removeClass("active")
         })
     }
+
+    $(".btns a").click(function(e) {
+        e.preventDefault();
+        $(".btns a").removeClass("active");
+        $(this).addClass("active");
+    
+        if ($(this).text() == "ALL") {
+            $(".html,.scss,.bootstrap").fadeIn();
+            
+        }
+        else if ($(this).text() == "HTML&CSS") {
+            $(".html").fadeIn();
+            $(".scss, .bootstrap").fadeOut();
+        }
+        else if($(this).text()=="BOOTSTRAP"){
+            $(".bootstrap").fadeIn();
+            $(".html,.scss").fadeOut();
+        }
+        else if($(this).text()=="SASS"){
+            $(".sass").fadeIn();
+            $(".html,.bootstrap").fadeOut();
+        }
+    });
+
+         
+
+    
 });
